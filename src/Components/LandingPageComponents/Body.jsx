19e8ca07cd6c37a964b86eecb93e117b8ccfe4b3
MuhamadRidwanPermana <a2.2000068@mhs.stmik-sumedang.ac.id>
2023-10-30 20:44:40 +0700

    fixed logo navbar

--- src/Components/LandingPageComponents/Body.jsx
+++ src/Components/LandingPageComponents/Body.jsx
@@ -1,32 +1,32 @@
-import { useState, useRef } from 'react'
+// import { useState, useRef } from 'react'
 
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
-import Logo from '../../assets/img/logo.svg';
-import Logo_SGP from '../../assets/img/LogoPremium.svg';
-import Image from '../../assets/img/image_landingpage.png';
-import Image2 from '../../assets/img/image_landingpage2.png';
-import IconItems from '../../assets/img/Icon_Items.svg';
-import IconFolder from '../../assets/img/Icon_Folder.svg';
-import IconDrone from '../../assets/img/Icon_Drone.svg';
-import IconHeadset from '../../assets/img/Icon_Headset.svg';
+// import Logo from '../../assets/img/logo.svg';
+// import Logo_SGP from '../../assets/img/LogoPremium.svg';
+// import Image from '../../assets/img/image_landingpage.png';
+// import Image2 from '../../assets/img/image_landingpage2.png';
+// import IconItems from '../../assets/img/Icon_Items.svg';
+// import IconFolder from '../../assets/img/Icon_Folder.svg';
+// import IconDrone from '../../assets/img/Icon_Drone.svg';
+// import IconHeadset from '../../assets/img/Icon_Headset.svg';
 
-import Subtract from '../../assets/img/Subtract.svg';
-import Subtract2 from '../../assets/img/Subtract2.svg';
-import Logo2 from '../../assets/img/Logo2.svg';
-import Rect from '../../assets/img/rect.svg';
+// import Subtract from '../../assets/img/Subtract.svg';
+// import Subtract2 from '../../assets/img/Subtract2.svg';
+// import Logo2 from '../../assets/img/Logo2.svg';
+// import Rect from '../../assets/img/rect.svg';
 
-import { FaArrowRightLong } from 'react-icons/fa6';
-import { BsCheck2 } from 'react-icons/bs';
-import { CgClose } from 'react-icons/cg';
+// import { FaArrowRightLong } from 'react-icons/fa6';
+// import { BsCheck2 } from 'react-icons/bs';
+// import { CgClose } from 'react-icons/cg';
 
-export default function Body() {
+// export default function Body() {
 
-  const ref = useRef(null);
+//   const ref = useRef(null);
 
-  return (
-    <>
+//   return (
+//     <>
       
-    </>
-  )
-}
+//     </>
+//   )
+// }

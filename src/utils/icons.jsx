8d4ffe26e21f03254de08faee1e8bb38315de749
MuhamadRidwanPermana <a2.2000068@mhs.stmik-sumedang.ac.id>
2023-10-30 21:41:01 +0700

    update icon

--- src/utils/icons.jsx
+++ src/utils/icons.jsx
@@ -1,16 +1,6 @@
 export { default as IconApp } from '../assets/img/app_icon.jpg';
-export { default as LogoApp } from '../assets/img/Logo.svg';
-export { default as Logo } from '../assets/img/Logo2.svg';
-export { default as LogoPremium } from '../assets/img/LogoPremium.svg';
 export { default as Image } from '../assets/img/image_landingpage.png';
 export { default as Image2 } from '../assets/img/image_landingpage2.png';
-export { default as IconDrone } from '../assets/img/Icon_Drone.svg';
-export { default as IconFolder } from '../assets/img/Icon_Folder.svg';
-export { default as IconHeadset } from '../assets/img/Icon_Headset.svg';
-export { default as IconItems } from '../assets/img/Icon_Items.svg';
-export { default as IconSubtract } from '../assets/img/Subtract.svg';
-export { default as IconSubtract2 } from '../assets/img/Subtract2.svg';
-export { default as IconRect } from '../assets/img/Rect.svg';
 export { default as Image_Admin } from '../assets/img/admin.png';
 
 export { RxHamburgerMenu } from "react-icons/rx";
